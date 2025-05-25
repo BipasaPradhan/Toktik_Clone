ec52b68a1b001f0c745a4afc421eedbe67e9e1eb
Bipasa Pradhan <bipspradhan45@gmail.com>
2025-05-26 02:28:55 +0700

    added in redis support

--- backend/src/main/java/com/example/securingweb/SecuringWebApplication.java
+++ backend/src/main/java/com/example/securingweb/SecuringWebApplication.java
@@ -2,8 +2,10 @@ package com.example.securingweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class SecuringWebApplication {
 
 	public static void main(String[] args) throws Throwable {
