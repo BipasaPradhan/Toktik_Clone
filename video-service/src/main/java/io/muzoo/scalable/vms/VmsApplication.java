186ca86434607995c89f397fc1e0fe09254bf931
Yueh-Hsuan Wu <yuehhsuan.wu@student.mahidol.edu>
2025-05-28 17:51:06 +0700

    trying to integrate Redis - Nanu

--- video-service/src/main/java/io/muzoo/scalable/vms/VmsApplication.java
+++ video-service/src/main/java/io/muzoo/scalable/vms/VmsApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication(exclude = {
 		DataSourceAutoConfiguration.class,
 		DataSourceTransactionManagerAutoConfiguration.class,
 		HibernateJpaAutoConfiguration.class
 })
+@EnableRedisHttpSession
 public class VmsApplication {
 
 	public static void main(String[] args) {
