02db8c6f5199b0049c31d4bc9b8074508613c210
Yueh-Hsuan Wu <yuehhsuan.wu@student.mahidol.edu>
2025-06-01 18:55:27 +0700

    removed unnecessary files

--- video-service/src/main/java/io/muzoo/scalable/vms/r2/VideoController.java
+++ video-service/src/main/java/io/muzoo/scalable/vms/r2/VideoController.java
@@ -2,7 +2,6 @@ package io.muzoo.scalable.vms.r2;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
