2bd8f4fd9dd330ae28566249440a7ef4e5c0b886
Yueh-Hsuan Wu <yuehhsuan.wu@student.mahidol.edu>
2025-06-02 19:18:34 +0700

    added logging & made sure workers communicate back - Nanu

--- video-service/src/main/java/io/muzoo/scalable/vms/config/RedisConfig.java
+++ video-service/src/main/java/io/muzoo/scalable/vms/config/RedisConfig.java
@@ -1,6 +1,9 @@
 package io.muzoo.scalable.vms.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.muzoo.scalable.vms.redis.RedisMessageListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -13,6 +16,7 @@ import org.springframework.data.redis.listener.PatternTopic;
 
 @Configuration
 public class RedisConfig {
+    private static final Logger logger = LoggerFactory.getLogger(RedisConfig.class);
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
@@ -29,11 +33,17 @@ public class RedisConfig {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
         container.addMessageListener(listenerAdapter, new PatternTopic("video:processed"));
+        container.setErrorHandler(e -> logger.error("Error in Redis listener container: {}", e.getMessage(), e));
         return container;
     }
 
     @Bean
     public MessageListenerAdapter listenerAdapter(RedisMessageListener listener) {
         return new MessageListenerAdapter(listener, "onMessage");
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 }
